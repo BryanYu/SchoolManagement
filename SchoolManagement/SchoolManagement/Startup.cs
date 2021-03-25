901433b555d2164a4c34b64cf9eab3926b1b9bc5
Bryan.Yu <bryan.yu@johnsontechinc.com>
2021-03-25 16:33:35 +0800

    Asp.net Core Middleware app.Run and app.Use

--- SchoolManagement/SchoolManagement/Startup.cs
+++ SchoolManagement/SchoolManagement/Startup.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace SchoolManagement
 {
@@ -26,31 +27,48 @@ namespace SchoolManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Run(async (context) =>
+            //{
+            //    context.Response.ContentType = "text/plain;charset=utf-8";
+            //    await context.Response.WriteAsync("從第一個Middleware的Hello world");
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    context.Response.ContentType = "text/plain;charset=utf-8";
+            //    await context.Response.WriteAsync("從第二個Middleware的Hello world");
+            //});
+
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1:傳入請求");
+                await next();
+                logger.LogInformation("MW1:回應請求");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2:傳入請求");
+                await next();
+                logger.LogInformation("MW2:回應請求");
+
+            });
+
             app.Run(async (context) =>
             {
                 context.Response.ContentType = "text/plain;charset=utf-8";
-                await context.Response.WriteAsync(_config["MyKey"]);
+                await context.Response.WriteAsync("MW3:處理請求並生成回應");
+                logger.LogInformation("MW3:處理請求並生成回應");
             });
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
-                    await context.Response.WriteAsync(processName);
-                });
-            });
-
-            
         }
     }
 }
