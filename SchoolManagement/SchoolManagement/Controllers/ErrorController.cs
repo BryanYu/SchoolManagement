367b856d82bd17f82f2e3b894fe0e6f4819db79b
Bryan.Yu <bryan.yu@johnsontechinc.com>
2021-04-11 18:15:53 +0800

    Logger

--- SchoolManagement/SchoolManagement/Controllers/ErrorController.cs
+++ SchoolManagement/SchoolManagement/Controllers/ErrorController.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace SchoolManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
@@ -17,26 +25,23 @@ namespace SchoolManagement.Controllers
             switch (statusCode)
             {
                 case 404:
+                    this._logger.LogWarning(
+                        $"發生了404錯誤. 路徑:{statusCodeResult.OriginalPath}, 查詢字串:{statusCodeResult.OriginalQueryString}");
+
                     ViewBag.ErrorMessage = "抱歉 該頁面不存在";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QS = statusCodeResult.OriginalQueryString;
                     break;
                     
             }
 
             return View("NotFound");
-            {
-                
-            }
+            
         }
 
         [Route("Error")]
         public IActionResult Error()
         {
             var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
-            ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
-            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+            this._logger.LogError($"路徑{exceptionHandlerPathFeature.Path} 產生了一個錯誤 {exceptionHandlerPathFeature.Error}");
             return View("Error");
         }
     }
