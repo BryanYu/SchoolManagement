6e7203c5e723db898ab6ef0d6aa803dbb99f3a5b
Bryan.Yu <bryan.yu@johnsontechinc.com>
2021-05-10 11:31:13 +0800

    Commit

---
+++ SchoolManagement/SchoolManagement/Application/Dtos/PagedSortedAndFilterInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Application.Dtos
+{
+    public class PagedSortedAndFilterInput
+    {
+        public PagedSortedAndFilterInput()
+        {
+            CurrentPage = 1;
+            MaxResultCount = 10;
+        }
+
+        [Range(0,1000)]
+        public int MaxResultCount { get; set; }
+
+        [Range(0,1000)]
+        public int CurrentPage { get; set; }
+
+        public string Sorting { get; set; }
+
+        public string FilterText { get; set; }
+    }
+
+    
+}
